97543c5fd4bc4c70e0fddaa8d28b4e91234af966
wenbin.shi <swbsin@163.com>
2018-09-25 02:22:51 -0700

    verify dynamic validators

--- app/utils.go
+++ app/utils.go
@@ -51,14 +51,70 @@ func (app *EthermintApplication) SetValidators(validators []*abciTypes.Validator
 // GetUpdatedValidators returns an updated validator set from the strategy
 // #unstable
 func (app *EthermintApplication) GetUpdatedValidators(height int64) abciTypes.ResponseEndBlock {
-	//if app.strategy != nil {
-	//	var validatorsSlice []abciTypes.Validator
-	//	validators := app.strategy.GetUpdatedValidators()
-	//	for i:=0;i< len(validators);i++{
-	//		validatorsSlice=append(validatorsSlice,*validators[i])
-	//	}
-	//	return abciTypes.ResponseEndBlock{ValidatorUpdates: validatorsSlice}
-	//}
+	if app.strategy != nil {
+		if height == 1 {
+			var validatorsSlice []abciTypes.Validator
+			validators := app.strategy.GetUpdatedValidators()
+			for i := 0; i < len(validators); i++ {
+				validatorsSlice = append(validatorsSlice,
+					abciTypes.Validator{
+						Address: validators[i].Address,
+						Power:   int64(0),
+						PubKey:  validators[i].PubKey,
+					})
+			}
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[0])
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[1])
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[2])
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[3])
+			return abciTypes.ResponseEndBlock{ValidatorUpdates: validatorsSlice}
+		} else if height > 1 && int(height)%3 == 1 {
+			var validatorsSlice []abciTypes.Validator
+			for i := 0; i < 4; i++ {
+				validatorsSlice = append(validatorsSlice,
+					abciTypes.Validator{
+						Address: app.candidateValidators[i+2].Address,
+						Power:   int64(0),
+						PubKey:  app.candidateValidators[i+2].PubKey,
+					})
+			}
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[0])
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[1])
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[2])
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[3])
+			return abciTypes.ResponseEndBlock{ValidatorUpdates: validatorsSlice}
+		} else if height > 1 && int(height)%3 == 2 {
+			var validatorsSlice []abciTypes.Validator
+			for i := 0; i < 4; i++ {
+				validatorsSlice = append(validatorsSlice,
+					abciTypes.Validator{
+						Address: app.candidateValidators[i].Address,
+						Power:   int64(0),
+						PubKey:  app.candidateValidators[i].PubKey,
+					})
+			}
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[1])
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[2])
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[3])
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[4])
+			return abciTypes.ResponseEndBlock{ValidatorUpdates: validatorsSlice}
+		} else if height > 1 && int(height)%3 == 0 {
+			var validatorsSlice []abciTypes.Validator
+			for i := 0; i < 4; i++ {
+				validatorsSlice = append(validatorsSlice,
+					abciTypes.Validator{
+						Address: app.candidateValidators[i+1].Address,
+						Power:   int64(0),
+						PubKey:  app.candidateValidators[i+1].PubKey,
+					})
+			}
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[2])
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[3])
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[4])
+			validatorsSlice = append(validatorsSlice, *app.candidateValidators[5])
+			return abciTypes.ResponseEndBlock{ValidatorUpdates: validatorsSlice}
+		}
+	}
 	return abciTypes.ResponseEndBlock{}
 }
 
