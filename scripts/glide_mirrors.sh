a07ba6c28a6b45c7ae29b1940446a40ccaaa2264
wenbin.shi <swbsin@163.com>
2018-10-30 06:45:19 -0700

    add glide_mirrors.sh

--- scripts/glide_mirrors.sh
+++ scripts/glide_mirrors.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+if [ -f "/root/.glide/mirrors.yaml" ];then
+echo "glide mirror.yaml exist"
+else
+touch /root/.glide/mirrors.yaml
+fi
+
+glide mirror set https://golang.org/x/crypto https://github.com/golang/crypto --vcs git
+
+glide mirror set https://golang.org/x/net https://github.com/golang/net --vcs git
+
+glide mirror set https://golang.org/x/sys https://github.com/golang/sys --vcs git
+
+glide mirror set https://golang.org/x/text https://github.com/golang/text --vcs git
+
+glide mirror set https://google.golang.org/grpc https://github.com/grpc/grpc-go --vcs git
+
+glide mirror set https://google.golang.org/genproto https://github.com/google/go-genproto --vcs git
