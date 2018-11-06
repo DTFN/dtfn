9c0d8b2e1b05dccb7f0a53ea3dc1e08d22c2f225
wenbin.shi <swbsin@163.com>
2018-11-05 21:34:07 -0800

    add interface

--- types/val_sortlist.go
+++ types/val_sortlist.go
@@ -1,1 +1,23 @@
 package types
+
+import "container/list"
+
+type ValSortlist struct {
+	ValList *list.List
+	Len     int
+}
+
+func NewValSortlist() *ValSortlist {
+	return &ValSortlist{
+		ValList: list.New(),
+		Len:     0,
+	}
+}
+
+func (vallist *ValSortlist) UpsertVal() {
+
+}
+
+func (vallist *ValSortlist) RemoveVal() {
+
+}
