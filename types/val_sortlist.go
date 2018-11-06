74ccd00d0712b82366b659f69ebfbcab9bc9099b
wenbin.shi <swbsin@163.com>
2018-11-05 23:05:03 -0800

    complete valsortlist

--- types/val_sortlist.go
+++ types/val_sortlist.go
@@ -1,12 +1,22 @@
 package types
 
 import (
+	"bytes"
 	"container/list"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/tendermint/tendermint/abci/types"
+	"math/big"
 )
 
 type ValSortlist struct {
 	ValList *list.List
 	Len     int
+}
+
+type ValListItem struct {
+	Signer     common.Address `json:"signer"`
+	Balance    *big.Int       `json:"balance"`
+	AbciPubkey types.PubKey   `json:"abciPubkey"`
 }
 
 func NewValSortlist() *ValSortlist {
@@ -16,15 +26,46 @@ func NewValSortlist() *ValSortlist {
 	}
 }
 
-func (valSortlist *ValSortlist) UpsertVal(posItem *PosItem, existFlag bool) {
+func (valSortlist *ValSortlist) UpsertVal(valListItem *ValListItem, existFlag bool) {
 	if existFlag {
-		valSortlist.ValList.PushFront(posItem)
-	} else {
-		valSortlist.ValList.PushFront(posItem)
-		valSortlist.Len = valSortlist.Len + 1
+		currentEle := valSortlist.ValList.Front()
+		currentItem := valSortlist.ValList.Front().Value.(*ValListItem)
+		for i := 0; i < valSortlist.Len; i++ {
+			if bytes.Equal(currentItem.Signer.Bytes(), valListItem.Signer.Bytes()) {
+				valSortlist.ValList.Remove(currentEle)
+			}
+		}
+	}
+	compareEle := valSortlist.ValList.Front()
+	compareItem := compareEle.Value.(*ValListItem)
+	for i := 0; i < valSortlist.Len; i++ {
+		if valListItem.Balance.Cmp(compareItem.Balance) >= 0 {
+			valSortlist.ValList.InsertBefore(valListItem, compareEle)
+		}
 	}
 }
 
-func (vallist *ValSortlist) RemoveVal(posItem *PosItem) {
+func (vallist *ValSortlist) RemoveVal(valListItem *ValListItem) {
+	currentEle := vallist.ValList.Front()
+	currentItem := vallist.ValList.Front().Value.(*ValListItem)
+	for i := 0; i < vallist.Len; i++ {
+		if bytes.Equal(currentItem.Signer.Bytes(), valListItem.Signer.Bytes()) {
+			vallist.ValList.Remove(currentEle)
+		}
+	}
+}
 
+func (vallist *ValSortlist) GetTop100ValPubkey() map[types.PubKey]bool {
+	pubKeyMap := make(map[types.PubKey]bool)
+	currentEle := vallist.ValList.Front()
+	currentItem := vallist.ValList.Front().Value.(*ValListItem)
+	for i := 0; i < vallist.Len; i++ {
+		if i == 100 {
+			break
+		}
+		pubKeyMap[currentItem.AbciPubkey] = true
+		currentEle = currentEle.Next()
+		currentItem = currentEle.Value.(*ValListItem)
+	}
+	return pubKeyMap
 }
