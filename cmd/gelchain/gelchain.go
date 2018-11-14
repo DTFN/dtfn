b3185e6d2991362108614c4b190e9135ea68f371
wenbin.shi <swbsin@163.com>
2018-11-14 04:42:26 -0800

    add for gelchain-pos

--- cmd/gelchain/gelchain.go
+++ cmd/gelchain/gelchain.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/green-element-chain/gelchain/utils"
 	"gopkg.in/urfave/cli.v1"
 	"math/big"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/ethereum/go-ethereum/accounts"
@@ -96,8 +96,7 @@ func ethermintCmd(ctx *cli.Context) error {
 		log.Info("get Initial accounts")
 		for i := 0; i < len(validators); i++ {
 			tmAddress = append(tmAddress, strings.ToLower(hex.EncodeToString(validators[i].PubKey.Address())))
-			blsKey := validators[i].BlsPubKey
-			blsKeyJsonStr, _ := json.Marshal(blsKey)
+			blsKeyJsonStr := strconv.Itoa(i)
 			accountBalance := big.NewInt(1)
 			accountBalance.Div(totalBalanceInital, big.NewInt(100))
 			if i == len(ethAccounts.EthAccounts) {
