9a390bd6d1a84a484fabfc8c6f9af4855a14795f
JamesRay <66258875@qq.com>
2019-06-26 18:38:57 +0800

    fix ethTransaction in gelchain

--- types/ethTransaction.go
+++ types/ethTransaction.go
@@ -3,20 +3,36 @@ package types
 import "github.com/ethereum/go-ethereum/common"
 import "github.com/ethereum/go-ethereum/core/types"
 import "github.com/ethereum/go-ethereum/rlp"
-import "io"
+import (
+	"io"
+	"bytes"
+)
 
 type EthTransaction struct {
 	Tx   *types.Transaction
 	From common.Address
 }
 
+type EthTransactionRLP struct {
+	TxData []byte
+	From   common.Address
+}
+
 // EncodeRLP implements rlp.Encoder
-func (tx *EthTransaction) EncodeRLP(w io.Writer) error {
-	return rlp.Encode(w, tx)
+func (ethTx *EthTransaction) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, &EthTransactionRLP{ethTx.Tx.Data(), ethTx.From})
 }
 
 // DecodeRLP implements rlp.Decoder
-func (tx *EthTransaction) DecodeRLP(s *rlp.Stream) error {
-	err := s.Decode(tx)
-	return err
+func (ethTx *EthTransaction) DecodeRLP(s *rlp.Stream) error {
+	var dec EthTransactionRLP
+	if err := s.Decode(&dec); err != nil {
+		return err
+	}
+	ethTx.From = dec.From
+	tx := new(types.Transaction)
+	rlpStream := rlp.NewStream(bytes.NewBuffer(dec.TxData), 0)
+	tx.DecodeRLP(rlpStream)
+	ethTx.Tx = tx
+	return nil
 }
