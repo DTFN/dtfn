b2b4a50521a6d7f181de8f214debe589bf0c4dd1
root <root@ip-172-31-33-64.ap-southeast-1.compute.internal>
2020-04-16 02:44:11 +0000

    buildAll.sh

--- scripts/buildAll.sh
+++ scripts/buildAll.sh
@@ -1,24 +1,41 @@
 #!/usr/bin/env bash
 installEthereum(){
-    mkdir -p $GOPATH/src/github.com/ethereum  && cd $_
-    git clone git@github.com:green-element-chain/go-ethereum.git
-    cd go-ethereum
+    if [ -d $GOPATH/src/github.com/ethereum/go-ethereum ]
+        then
+                cd $GOPATH/src/github.com/ethereum/go-ethereum
+                git pull
+        else
+        	mkdir -p $GOPATH/src/github.com/ethereum  && cd $GOPATH/src/github.com/ethereum
+        	pwd
+        	git clone https://github.com/green-element-chain/go-ethereum.git
+        	cd go-ethereum
+        	pwd
+    fi
+
     git checkout ForbidNormalPeer
     go mod vendor
     cd cmd/geth && go install
     cd ../../../..
+    pwd
 }
 
 
 installTendermint(){
-    mkdir tendermint && cd tendermint/
-    git clone git@github.com:green-element-chain/tendermint.git
-    cd tendermint
+    if [ -d $GOPATH/src/github.com/tendermint/tendermint ]
+            then
+                    cd $GOPATH/src/github.com/tendermint/tendermint
+                    git pull
+            else
+            	mkdir -p $GOPATH/src/github.com/tendermint  && cd $GOPATH/src/github.com/tendermint
+            	git clone https://github.com/green-element-chain/tendermint.git
+            	cd tendermint
+    fi
     #git checkout -b develop remotes/origin/develop
     git checkout ForbidNormalPeer
     go mod vendor
     cd cmd/tendermint && go install
     cd ../../../..
+    pwd
 }
 
 installGelchain(){
@@ -36,27 +53,33 @@ installGelchain(){
 
 backupVendors(){
     mkdir vendor_bak
-    mv ../../ethereum/go-etherem/vendor/github.com/tendermint/tendermint vendor_bak/ethereum/go-etherem/vendor/github.com/tendermint/tendermint
-    mv vendor/github.com/tendermint/tendermint vendor_bak/green-element-chain/gelchain/vendor/github.com/tendermint/tendermint
-    mv ../../tendermint/tendermint/vendor/github.com/ethereum/go-ethereum vendor_bak/tendermint/tendermint/vendor/github.com/ethereum/go-ethereum
-    mv vendor/github.com/ethereum/go-ethereum vendor_bak/green-element-chain/gelchain/vendor/github.com/ethereum/go-ethereum
-    rm ../../ethereum/go-etherem/vendor/github.com/karalabe
-    rm ../../ethereum/go-etherem/vendor/gopkg.in -rf
+    mkdir -p vendor_bak/ethereum/go-ethereum/vendor/github.com/tendermint
+    mv ../../ethereum/go-ethereum/vendor/github.com/tendermint/tendermint vendor_bak/ethereum/go-ethereum/vendor/github.com/tendermint
+    mkdir -p vendor_bak/green-element-chain/gelchain/vendor/github.com/tendermint
+    mv vendor/github.com/tendermint/tendermint vendor_bak/green-element-chain/gelchain/vendor/github.com/tendermint
+    mkdir -p vendor_bak/tendermint/tendermint/vendor/github.com/ethereum
+    mv ../../tendermint/tendermint/vendor/github.com/ethereum/go-ethereum vendor_bak/tendermint/tendermint/vendor/github.com/ethereum
+    mkdir -p vendor_bak/green-element-chain/gelchain/vendor/github.com/ethereum
+    mv vendor/github.com/ethereum/go-ethereum vendor_bak/green-element-chain/gelchain/vendor/github.com/ethereum
+    rm ../../ethereum/go-ethereum/vendor/github.com/karalabe -rf
+    rm ../../ethereum/go-ethereum/vendor/gopkg.in -rf
     rm ../../../gopkg.in -rf
-    mv vendor/gopkg.in ../../.. -rf
+    mv vendor/gopkg.in ../../.. -f
 }
 
 
 restoreVendors(){
-    mv vendor_bak/ethereum/go-etherem/vendor/github.com/tendermint/tendermint ../../ethereum/go-etherem/vendor/github.com/tendermint
+    mv vendor_bak/ethereum/go-ethereum/vendor/github.com/tendermint/tendermint ../../ethereum/go-ethereum/vendor/github.com/tendermint
     mv vendor_bak/green-element-chain/gelchain/vendor/github.com/tendermint/tendermint vendor/github.com/tendermint
     mv vendor_bak/tendermint/tendermint/vendor/github.com/ethereum/go-ethereum ../../tendermint/tendermint/vendor/github.com/ethereum
     mv vendor_bak/green-element-chain/gelchain/vendor/github.com/ethereum/go-ethereum vendor/github.com/ethereum
-    cp ../../karalabe ../../ethereum/go-etherem/vendor/github.com/ -rf
-    cp ../../../gopkg.in vendor ../../ethereum/go-etherem/vendor -rf
+    cp ../../karalabe ../../ethereum/go-ethereum/vendor/github.com/ -rf
+    cp ../../../gopkg.in vendor ../../ethereum/go-ethereum/vendor -rf
+    rm vendor_bak -rf
 }
 
 export GO111MODULE=on
 installEthereum
 installTendermint
 installGelchain
+
